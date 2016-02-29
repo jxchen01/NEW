bdd63c522d4fc485cc7749d5d7e6712dda897c5f
jxchen01 <jchen16@nd.edu>
2016-02-29 15:34:47 -0500

    test

--- unet_test.lua
+++ unet_test.lua
@@ -50,42 +50,59 @@ L5c=nn.ReLU(true)(L5b)
 L5d=nn.SpatialConvolution(1024, 1024, 3, 3, 1, 1, 0, 0)(L5c)
 L5=nn.ReLU(true)(L5d)
 
+--[[
 Crop4=nn.Narrow(2,4,2*XX-4)(L4)
 L4cp=nn.Narrow(3,4,2*XX-4)(Crop4)
+--]]
 L5up=nn.SpatialFullConvolution(1024, 512, 2, 2, 2, 2)(L5)
 
-L6a=nn.JoinTable(1,3)({L5up,L4cp})
-L6b=nn.SpatialConvolution(1024,512, 3, 3, 1, 1, 0, 0)(L6a)
+--L6a=nn.JoinTable(1,3)({L5up,L4cp})
+--L6b=nn.SpatialConvolution(1024,512, 3, 3, 1, 1, 0, 0)(L6a)
+
+L6b=nn.SpatialConvolution(1024,512, 3, 3, 1, 1, 0, 0)(L5up)
 L6c=nn.ReLU(true)(L6b)
 L6d=nn.SpatialConvolution(512,512, 3, 3, 1, 1, 0, 0)(L6c)
 L6=nn.ReLU(true)(L6d)
 
+--[[
 Crop3=nn.Narrow(2,16,4*XX-16)(L3)
 L3cp=nn.Narrow(3,16,4*XX-16)(Crop3)
+--]]
 L6up=nn.SpatialFullConvolution(512, 256, 2, 2, 2, 2)(L6)
 
-L7a=nn.JoinTable(1,3)({L6up,L3cp})
-L7b=nn.SpatialConvolution(512,256, 3, 3, 1, 1, 0, 0)(L7a)
+--L7a=nn.JoinTable(1,3)({L6up,L3cp})
+--L7b=nn.SpatialConvolution(512,256, 3, 3, 1, 1, 0, 0)(L7a)
+
+L7b=nn.SpatialConvolution(512,256, 3, 3, 1, 1, 0, 0)(L6up)
 L7c=nn.ReLU(true)(L7b)
 L7d=nn.SpatialConvolution(256,256, 3, 3, 1, 1, 0, 0)(L7c)
 L7=nn.ReLU(true)(L7d)
 
+--[[
 Crop2=nn.Narrow(2,40,8*XX-40)(L2)
 L2cp=nn.Narrow(3,40,8*XX-40)(Crop2)
+--]]
 L7up=nn.SpatialFullConvolution(256, 128, 2, 2, 2, 2)(L7)
 
-L8a=nn.JoinTable(1,3)({L7up,L2cp})
-L8b=nn.SpatialConvolution(256,128, 3, 3, 1, 1, 0, 0)(L8a)
+--L8a=nn.JoinTable(1,3)({L7up,L2cp})
+--L8b=nn.SpatialConvolution(256,128, 3, 3, 1, 1, 0, 0)(L8a)
+
+L8b=nn.SpatialConvolution(256,128, 3, 3, 1, 1, 0, 0)(L7up)
 L8c=nn.ReLU(true)(L8b)
 L8d=nn.SpatialConvolution(128,128, 3, 3, 1, 1, 0, 0)(L8c)
 L8=nn.ReLU(true)(L8d)
 
+--[[
 Crop1=nn.Narrow(2,88,16*XX-88)(L1)
 L1cp=nn.Narrow(3,88,16*XX-88)(Crop1)
+--]]
 L8up=nn.SpatialFullConvolution(128, 64, 2, 2, 2, 2)(L8)
 
-L9a=nn.JoinTable(1,3)({L8up,L1cp})
-L9b=nn.SpatialConvolution(128,64, 3, 3, 1, 1, 0, 0)(L9a)
+
+--L9a=nn.JoinTable(1,3)({L8up,L1cp})
+--L9b=nn.SpatialConvolution(128,64, 3, 3, 1, 1, 0, 0)(L9a)
+L9b=nn.SpatialConvolution(128,64, 3, 3, 1, 1, 0, 0)(L8up)
+
 L9c=nn.ReLU(true)(L9b)
 L9d=nn.SpatialConvolution(64,64, 3, 3, 1, 1, 0, 0)(L9c)
 L9=nn.ReLU(true)(L9d)
