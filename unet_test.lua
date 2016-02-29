7b8090585a9b58d8238d8d484a700cdf2b5557ec
jxchen01 <jchen16@nd.edu>
2016-02-29 17:01:59 -0500

    test1

--- unet_test.lua
+++ unet_test.lua
@@ -26,13 +26,13 @@ L1b=nn.ReLU(true)(L1a)
 L1c=nn.SpatialConvolution(64, 64, 3, 3, 1, 1, 0, 0)(L1b)
 L1=nn.ReLU(true)(L1c)
 
+--[[
+
 L2a=nn.SpatialMaxPooling(2, 2, 2, 2)(L1)
 L2b=nn.SpatialConvolution(64, 128, 3, 3, 1, 1, 0, 0)(L2a)
 L2c=nn.ReLU(true)(L2b)
 L2d=nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 0, 0)(L2c)
 L2=nn.ReLU(true)(L2d)
-
---[[
 
 L3a=nn.SpatialMaxPooling(2, 2, 2, 2)(L2)
 L3b=nn.SpatialConvolution(128, 256, 3, 3, 1, 1, 0, 0)(L3a)
